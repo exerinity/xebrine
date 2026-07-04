3b985d8a1d77f0836e6f590a9dd91b12774af9de
exerinity <me@exerinity.com>
2026-07-04 17:25:22 +1000

    m

---
+++ src/pages/lyrics.tsx
@@ -0,0 +1,26 @@
+import { usePlayer } from '../context/player_context';
+import { LyricsPanel } from '../components/lyrics';
+import { ScrollingText } from '../components/scrolling_text';
+import { NoteIcon } from '../components/icons';
+
+export function LyricsPage() {
+  const { current, artworkUrl } = usePlayer();
+  const track = current?.track ?? null;
+
+  return (
+    <div className="xe_page">
+      {track && (
+        <div className="xe_lyrics-header">
+          <div className="xe_lyrics-header__art">
+            {artworkUrl ? <img src={artworkUrl} alt="" /> : <NoteIcon size={32} />}
+          </div>
+          <div className="xe_lyrics-header__titles">
+            <ScrollingText text={track.title} className="xe_lyrics-header__title" />
+            <ScrollingText text={`${track.artist} / ${track.album}`} className="xe_lyrics-header__subtitle" />
+          </div>
+        </div>
+      )}
+      <LyricsPanel />
+    </div>
+  );
+}
