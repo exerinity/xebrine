a80beb81a51d1676922abb193069d86b9a5eecba
exerinity <me@exerinity.com>
2026-07-10 21:15:10 +1000

    m

---
+++ src/hooks/explicit.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { isExplicitId, subscribeExplicit } from '../utils/explicit_tracks';
+
+export function useIsExplicit(trackId: string): boolean {
+  const [explicit, setExplicit] = useState(() => isExplicitId(trackId));
+
+  useEffect(() => {
+    setExplicit(isExplicitId(trackId));
+    return subscribeExplicit(() => setExplicit(isExplicitId(trackId)));
+  }, [trackId]);
+
+  return explicit;
+}
