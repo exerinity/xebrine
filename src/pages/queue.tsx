91c885ecc91ced051052ca3a5e6b988d4001783e
exerinity <me@exerinity.com>
2026-07-12 02:52:22 +1000

    m

--- src/pages/queue.tsx
+++ src/pages/queue.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { usePlayer } from '../context/player_context';
 import { QueueList } from '../components/queue_list';
 import { formatTime } from '../utils/format';
 import { usePageTitle } from '../hooks/page_title';
-import { ShuffleIcon, TrashIcon } from '../components/icons';
+import { SearchIcon, ShuffleIcon, TrashIcon } from '../components/icons';
 
 export function QueuePage() {
   const { queue, current, shuffled, toggleShuffle, clearQueue, clearOthers } = usePlayer();
   const totalSeconds = queue.reduce((sum, item) => sum + item.track.duration, 0);
+  const [query, setQuery] = useState('');
   usePageTitle('Queue');
 
   return (
@@ -16,6 +18,16 @@ export function QueuePage() {
         <span className="xe_page__meta">
           currently {queue.length} track{queue.length === 1 ? '' : 's'} enqueued, {formatTime(totalSeconds)} total time
         </span>
+        <div className="xe_search-field">
+          <SearchIcon size={14} />
+          <input
+            className="xe_search-input"
+            type="search"
+            placeholder="Search queue..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
         <button
           type="button"
           className={`xe_btn${shuffled ? ' xe_btn--accent' : ''}`}
@@ -40,7 +52,7 @@ export function QueuePage() {
         </button>
       </div>
       <div className="xe_page__scroll">
-        <QueueList />
+        <QueueList query={query} />
       </div>
     </div>
   );
