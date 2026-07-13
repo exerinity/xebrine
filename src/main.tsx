e9f7dec9eca954c39067bde4007d1cc7433ed60c
exerinity <me@exerinity.com>
2026-07-14 04:36:10 +1000

    m

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { registerSW } from 'virtual:pwa-register';
 import App from './app';
 import './stylesheet/index.css';
-
-registerSW({ immediate: true });
 
 createRoot(document.getElementById('xebrine')!).render(
   <StrictMode>
