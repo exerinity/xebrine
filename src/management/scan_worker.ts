a8ff51a6c91f54d02c58943c7a67440abafc636c
exerinity <me@exerinity.com>
2026-07-17 04:48:57 +1000

    m

--- src/management/scan_worker.ts
+++ src/management/scan_worker.ts
@@ -1,4 +1,4 @@
-import { fallbackTags, MAX_PARSE_FILE_SIZE, readTrackTags } from './metadata';
+import { fallbackTags, readTrackTags } from './metadata';
 
 interface ParseRequest {
   id: number;
@@ -10,15 +10,6 @@ self.onmessage = async (e: MessageEvent<ParseRequest>) => {
   const post = (msg: object) => (self as unknown as Worker).postMessage(msg);
   try {
     const file = await handle.getFile();
-    if (file.size > MAX_PARSE_FILE_SIZE) {
-      post({
-        id,
-        tags: fallbackTags(handle.name),
-        warning: 'oversized',
-        sizeMB: Math.round(file.size / 1024 / 1024)
-      });
-      return;
-    }
     post({ id, tags: await readTrackTags(file) });
   } catch {
     post({ id, tags: fallbackTags(handle.name), warning: 'unreadable' });
