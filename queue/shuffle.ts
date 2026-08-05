f1f9458ef75c5f5d02922f2481fd28bfd822abb7
exerinity <me@exerinity.com>
2026-08-05 15:41:39 +1000

    m

--- queue/shuffle.ts
+++ queue/shuffle.ts
@@ -3,6 +3,15 @@ interface ShuffleInfo {
   artist: string;
 }
 
+export function jumble<T>(items: T[]): T[] {
+  const out = [...items];
+  for (let i = out.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [out[i], out[j]] = [out[j], out[i]];
+  }
+  return out;
+}
+
 export function intelligentShuffle<T>(
   items: T[],
   info: (item: T) => ShuffleInfo,
