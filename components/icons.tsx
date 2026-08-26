6286be69d5d58ecc6075edad05627e417483066f
exerinity <me@exerinity.com>
2026-08-27 03:00:45 +1000

    m

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,3 +1,5 @@
+import { XEBRINE_LOGO_PATH, XEBRINE_LOGO_VIEW_BOX } from '../utils/logo';
+
 interface IconProps {
   size?: number;
 }
@@ -319,8 +321,8 @@ export const RemoteIcon = (p: IconProps) => (
 );
 
 export const LogoIcon = ({ size = 20 }: IconProps) => (
-  <svg width={size} height={size} viewBox="70 260 563 545" fill="currentColor" aria-hidden="true">
-    <path d="M86 413c26 -8 54 -12 84 -12s58 4 85 12v318h-169v-131h150v-56c-20 -4 -42 -6 -66 -6c-29 0 -57 4 -84 12v-137zM617 552c-27 -8 -56 -12 -85 -12c-30 0 -58 4 -84 12v-137c26 -8 54 -12 84 -12c29 0 58 4 85 12v137zM447 415c-27 -8 -55 -12 -84 -12c-23 0 -44 2 -66 6v384h-19v-515c27 -8 56 -12 85 -12c30 0 58 4 84 12v137z" />
+  <svg width={size} height={size} viewBox={XEBRINE_LOGO_VIEW_BOX} fill="currentColor" aria-hidden="true">
+    <path d={XEBRINE_LOGO_PATH} />
   </svg>
 );
 
