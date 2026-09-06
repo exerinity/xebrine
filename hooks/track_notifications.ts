c8f8069e18ecd83618610f050326646592e86625
exerinity <me@exerinity.com>
2026-09-06 19:28:21 +1000

    m

--- hooks/track_notifications.ts
+++ hooks/track_notifications.ts
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { usePlayer } from '../context/player_context';
+import { usePlayer } from '../src/context/player';
 import { useSettings } from '../context/settings_context';
 
 export function useTrackNotifications(): void {
