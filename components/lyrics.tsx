3d0765a3df91862dab82e1bec237783c5f573fe8
exerinity <me@exerinity.com>
2026-09-06 19:28:21 +1000

    m

--- components/lyrics.tsx
+++ components/lyrics.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { usePlayer } from '../context/player_context';
+import { usePlayer } from '../src/context/player';
 import { useSettings } from '../context/settings_context';
 import {
   fetchLyrics,
