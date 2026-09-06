35683bd18a50c8d68783c9d7e3d6d6ccd5974c51
exerinity <me@exerinity.com>
2026-09-06 19:28:21 +1000

    m

--- pages/artist_detail.tsx
+++ pages/artist_detail.tsx
@@ -1,7 +1,7 @@
 import { useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useLibrary } from '../context/library_context';
-import { usePlayer } from '../context/player_context';
+import { usePlayer } from '../src/context/player';
 import { groupArtists, groupAlbums, type AlbumGroup } from '../utils/groups';
 import { intelligentShuffle } from '../queue/shuffle';
 import { getRecentIds } from '../queue/history';
