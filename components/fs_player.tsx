d0f98055898785461359ca939ebcc2adf8a6938c
exerinity <me@exerinity.com>
2026-09-06 19:28:20 +1000

    m

--- components/fs_player.tsx
+++ components/fs_player.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState, type MouseEvent, type PointerEvent } from 'react';
-import { usePlayer } from '../context/player_context';
+import { usePlayer } from '../src/context/player';
 import { useSettings } from '../context/settings_context';
 import { useKenBurns } from '../hooks/ken_burns';
 import { formatTime } from '../utils/format';
