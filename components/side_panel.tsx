f358c82628455ea2c2bca4c18431b21f34fe1a6d
exerinity <me@exerinity.com>
2026-09-06 19:28:21 +1000

    m

--- components/side_panel.tsx
+++ components/side_panel.tsx
@@ -7,7 +7,7 @@ import {
   type PointerEvent
 } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { usePlayer } from '../context/player_context';
+import { usePlayer } from '../src/context/player';
 import type { SidePanelView } from '../context/settings_context';
 import { formatTime } from '../utils/format';
 import { LyricsPanel } from './lyrics';
