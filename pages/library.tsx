153885427ad12838cb8c7e36f93ad1913b86d78e
exerinity <me@exerinity.com>
2026-09-08 20:55:43 +1000

    m

--- pages/library.tsx
+++ pages/library.tsx
@@ -16,6 +16,7 @@ import {
   matchesDeepSearch,
   type DeepSearchCriteria
 } from '../components/deep_search';
+import { Link } from 'react-router-dom';
 
 type LibrarySort = 'artist' | 'album' | 'title' | 'duration';
 
@@ -99,7 +100,8 @@ export function LibraryPage() {
         <p className="xe_empty-note">
           The browser you're using doesn't seem to support (or you have denied access to)
           the File System Access API, which is required for Xebrine to read your music files. Please
-          try again using a different browser that supports it. In the meantime, <a href="https://voxity.dev" target="_blank">try Voxity</a>?
+          try again using a different browser that supports it. In the meantime, <a href="https://voxity.dev" target="_blank">try Voxity</a>
+          or <Link to="/radio">the radio</Link>?
         </p>
       </div>
     );
