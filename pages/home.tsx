239b4ae31f250e3fc3148bb621065bd387c8aa2b
exerinity <me@exerinity.com>
2026-09-08 20:55:43 +1000

    m

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useLibrary } from '../context/library_context';
 import { usePlayer } from '../src/context/player';
 import { intelligentShuffle } from '../queue/shuffle';
@@ -120,7 +120,8 @@ export function HomePage() {
         <p className="xe_empty-note">
           The browser you're using doesn't seem to support (or you have denied access to)
           the File System Access API, which is required for Xebrine to read your music files. Please
-          try again using a different browser that supports it. In the meantime, <a href="https://voxity.dev" target="_blank">try Voxity</a>?
+          try again using a different browser that supports it. In the meantime, <a href="https://voxity.dev" target="_blank">try Voxity</a>
+          or <Link to="/radio">the radio</Link>?
         </p>
       </div>
     );
